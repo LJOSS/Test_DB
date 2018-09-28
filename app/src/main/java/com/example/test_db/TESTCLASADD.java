737ea6c60df3d79c980fcb1ac9c3678873ae6c55
LJOSS <ivanbestboy@gmail.com>
2018-09-28 21:51:30 +0300

    Add test class and vse

---
+++ app/src/main/java/com/example/test_db/TESTCLASADD.java
@@ -0,0 +1,4 @@
+package com.example.test_db;
+
+public class TESTCLASADD {
+}
--- app/src/main/java/com/example/test_db/User_Activity.java
+++ app/src/main/java/com/example/test_db/User_Activity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 public class User_Activity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAdd;
-    EditText etName,etEmail,etId;
+    EditText etName, etEmail, etId;
     int Id;
     String Name, Email;
 
@@ -24,15 +24,32 @@ public class User_Activity extends AppCompatActivity implements View.OnClickList
         etEmail = findViewById(R.id.etEmail);
         etName = findViewById(R.id.etName);
         etId = findViewById(R.id.etId);
+
     }
+
+    public void sooqablyad() {
+        int i = 1;
+        int ii = 1;
+        int iii = 1;
+        int iiii = 1;
+        int iiiii = 1;
+    }
+
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btnAdd:
                 Id = Integer.parseInt(etId.getText().toString());
                 Name = etName.getText().toString();
                 Email = etEmail.getText().toString();
+
+                int i = 1;
+                int ii = 1;
+                int iii = 1;
+                int iiii = 1;
+                int iiiii = 1;
+
 
                 User user = new User();
                 user.setId(Id);
@@ -40,7 +57,11 @@ public class User_Activity extends AppCompatActivity implements View.OnClickList
                 user.setName(Name);
                 MainActivity.MyDB.userDAO().AddUser(user);
 
-                Toast.makeText(this,"Succes",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "SUQFDHIUFBIEUFBEUCQEF", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(this, "qwedasxcsxzcsc", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(this, "qweqwdasd", Toast.LENGTH_SHORT).show();
                 etEmail.setText("");
                 etName.setText("");
                 etId.setText("");
